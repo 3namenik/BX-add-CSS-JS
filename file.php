c70b7babbe5e9ede3a2f5b58f268ec34c7773527
3namenik <36602322+3namenik@users.noreply.github.com>
2023-05-17 15:33:30 +0300

    Update file.php

--- file.php
+++ file.php
@@ -1,9 +1,9 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 if (isset($this) && get_class($this) == 'CBitrixComponentTemplate') {
-    $this->addExternalCss(SITE_TEMPLATE_PATH . '/widgets/form-catalog/form-catalog.css');
-    $this->addExternalJS(SITE_TEMPLATE_PATH . '/widgets/form-catalog/form-catalog.js');
+    $this->addExternalCss(SITE_TEMPLATE_PATH . '/parts/form-catalog/form-catalog.css');
+    $this->addExternalJS(SITE_TEMPLATE_PATH . '/parts/form-catalog/form-catalog.js');
 } else {
-    Bitrix\Main\Page\Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/widgets/form-catalog/form-catalog.css');
-    Bitrix\Main\Page\Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/widgets/form-catalog/form-catalog.js');
+    Bitrix\Main\Page\Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/parts/form-catalog/form-catalog.css');
+    Bitrix\Main\Page\Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/parts/form-catalog/form-catalog.js');
 } ?>
